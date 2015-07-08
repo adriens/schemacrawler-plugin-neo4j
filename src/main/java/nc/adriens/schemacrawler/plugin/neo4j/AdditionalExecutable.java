d9ddcd7b906232eecd39a068a9f7176f9527f56d
salad74 <salad74@themint>
2015-07-08 15:38:11 +1100

    feedSchemas method added.

--- src/main/java/nc/adriens/schemacrawler/plugin/neo4j/AdditionalExecutable.java
+++ src/main/java/nc/adriens/schemacrawler/plugin/neo4j/AdditionalExecutable.java
@@ -1,12 +1,27 @@
 package nc.adriens.schemacrawler.plugin.neo4j;
 /*
-schemacrawler -host=localhost -port=5432 -database=sportsdb -user=sc -password=sc  -schemas=public -c=neo4j -infolevel=maximum -server=postgresql -loglevel=CONFIG -outputDir=c:/tmp
-*/
+http://www.tutorialspoint.com/neo4j/neo4j_native_java_api_example.htm
+http://www.sportsstandards.org/oc
+
+Search nodes :
+
+http://stackoverflow.com/questions/15368579/select-a-node-by-name-in-neo4j-in-java
+
+ schemacrawler -host=localhost -port=5432 -database=sportsdb -user=sc -password=sc  -schemas=public -c=neo4j -infolevel=maximum -server=postgresql -loglevel=CONFIG -outputDir=c:/tmp
+
+MATCH (a)-[r:CONTAINS_SCHEMA]->(b)
+RETURN r
+ */
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import org.apache.commons.io.FileUtils;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.Node;
 
 import schemacrawler.schema.Catalog;
 import schemacrawler.schema.Column;
@@ -14,20 +29,63 @@ import schemacrawler.schema.Schema;
 import schemacrawler.schema.Table;
 import schemacrawler.tools.executable.BaseStagedExecutable;
 
+public class AdditionalExecutable extends BaseStagedExecutable {
 
-public class AdditionalExecutable extends BaseStagedExecutable
-{
+    static final String COMMAND = "neo4j";
 
-  static final String COMMAND = "neo4j";
+    private String outputDir;
+    private GraphDatabaseFactory dbFactory;
+    private GraphDatabaseService dbService;
 
-  private String outputDir;
-  
-  protected AdditionalExecutable()
-  {
-    super(COMMAND);
-  }
+    protected AdditionalExecutable() {
+        super(COMMAND);
+    }
 
-  /**
+    public void init() throws IOException {
+        // recursively delete directory contents
+        System.out.println("Deleting directory <" + getOutputDir() + "> ...");
+        FileUtils.deleteDirectory(new File(getOutputDir()));
+        System.out.println("Output directory cleaned");
+        setDbFactory(new GraphDatabaseFactory());
+        setDbService(dbFactory.newEmbeddedDatabase(getOutputDir()));
+    }
+
+    public void feedSchemas(final Catalog catalog) {
+        try (Transaction tx = getDbService().beginTx()) {
+            for (final Schema schema : catalog.getSchemas()) {
+                Node schemaNode = dbService.createNode(DatabaseNodeType.SCHEMA);
+                schemaNode.setProperty("Name", schema.getName());
+                schemaNode.setProperty("FullName", schema.getFullName());
+                schemaNode.setProperty("LookupKey", schema.getLookupKey());
+                schemaNode.setProperty("Remarks", schema.getRemarks());
+                
+            }
+            tx.success();
+        }
+    }
+
+    @Override
+    public void executeOn(final Catalog catalog, final Connection connection)
+            throws Exception {
+        try (final PrintWriter writer = new PrintWriter(outputOptions.openNewOutputWriter());) {
+            setOutputDir(additionalConfiguration.getStringValue("outputDir", "neo4j"));
+            init();
+
+            for (final Schema schema : catalog.getSchemas()) {
+//        System.out.println(schema);
+//        for (final Table table: catalog.getTables(schema))
+//        {
+//          writer.println("o--> " + table);
+//          for (final Column column: table.getColumns())
+//          {
+//            writer.println("     o--> " + column);
+//          }
+                //}
+            }
+        }
+    }
+
+    /**
      * @return the outputDir
      */
     public String getOutputDir() {
@@ -40,35 +98,33 @@ public class AdditionalExecutable extends BaseStagedExecutable
     public void setOutputDir(String outputDir) {
         this.outputDir = outputDir;
     }
-    
-  @Override
-  public void executeOn(final Catalog catalog, final Connection connection)
-    throws Exception
-  {
-    try (final PrintWriter writer = new PrintWriter(outputOptions.openNewOutputWriter());)
-    {
-        System.out.println("NEO4J plugin !!!");
-        System.out.println("NEO4J output dir : " + additionalConfiguration.getStringValue("outputDir", "neo4j"));
-        setOutputDir(additionalConfiguration.getStringValue("outputDir", "neo4j"));
-        // recursively delete directory contents
-        System.out.println("Deleting directory <" + getOutputDir() + "> ...");
-        FileUtils.deleteDirectory(new File(getOutputDir()));
-        System.out.println("Output directory cleaned");
-      for (final Schema schema: catalog.getSchemas())
-      {
-//        System.out.println(schema);
-//        for (final Table table: catalog.getTables(schema))
-//        {
-//          writer.println("o--> " + table);
-//          for (final Column column: table.getColumns())
-//          {
-//            writer.println("     o--> " + column);
-//          }
-        //}
-      }
-    }
-  }
 
-    
+    /**
+     * @return the dbFactory
+     */
+    public GraphDatabaseFactory getDbFactory() {
+        return dbFactory;
+    }
+
+    /**
+     * @param dbFactory the dbFactory to set
+     */
+    public void setDbFactory(GraphDatabaseFactory dbFactory) {
+        this.dbFactory = dbFactory;
+    }
+
+    /**
+     * @return the dbService
+     */
+    public GraphDatabaseService getDbService() {
+        return dbService;
+    }
+
+    /**
+     * @param dbService the dbService to set
+     */
+    public void setDbService(GraphDatabaseService dbService) {
+        this.dbService = dbService;
+    }
 
 }
