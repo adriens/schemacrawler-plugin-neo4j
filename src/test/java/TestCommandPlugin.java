942589642790c16de0f3aa5f1361d22d8972dde2
adriens <Adrien.Sales@gmail.com>
2015-07-12 07:02:14 +1100

    Added command line test.

---
+++ src/test/java/TestCommandPlugin.java
@@ -0,0 +1,17 @@
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import schemacrawler.tools.executable.CommandRegistry;
+
+public class TestCommandPlugin
+{
+
+  @Test
+  public void testCommandPlugin()
+    throws Exception
+  {
+    final CommandRegistry registry = new CommandRegistry();
+    assertTrue(registry.hasCommand("neo4j"));
+  }
+
+}
